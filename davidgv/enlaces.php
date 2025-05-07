9de92ccafb47ba1c68357222f506a5567cf2cbf5
Davidgv22 <jesgonvic@alu.edu.gva.es>
2025-05-07 13:20:16 +0200

    Update and rename enlaces.html to enlaces.php

--- davidgv/enlaces.php
+++ davidgv/enlaces.php
@@ -1,6 +1,23 @@
 
 <!DOCTYPE html>
 <html lang="es">
+
+<?php
+ // Mostrar todos los errores (útil para debugging)
+ ini_set('display_errors', 1);
+ error_reporting(E_ALL);
+ // Conectar a la base de datos
+ // Cambiar el servername por el que proceda: localhost, IP, url, …
+ $servername = "127.0.0.1";
+ $username = "vicente";
+ $password = "p@ssw0rd";
+ $dbname = "camp";
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Verificar la conexión
+ if ($conn->connect_error) {
+ die("Conexión fallida: " . $conn->connect_error);
+ }
+?>
 <head>
   <meta charset="UTF-8">
   <title>Tabla de Autos</title>
@@ -31,10 +48,10 @@
     if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
     echo " <tr>
-    <td>{$row['razon_social']}</td>
-    <td>{$row['descripcion']}</td>
-    <td>{$row['email']}</td>
-    <td>{$row['telefono']}</td>
+    <td>{$row['cliente']}</td>
+    <td>{$row['coche']}</td>
+    <td>{$row['gama']}</td>
+    <td>{$row['reserva']}</td>
     </tr>";
     }
     }
